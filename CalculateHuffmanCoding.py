564aeda433875bfb20fc84e4c3bf3b2f39ff524b
Jan Feys <feys.jan@gmail.com>
2017-10-11 15:41:54 -0400

    Implemented recursive Huffman coding algorithm for case of distinct frequencies

---
+++ CalculateHuffmanCoding.py
@@ -0,0 +1,65 @@
+import sys
+from collections import defaultdict
+from heapq import heappop, heappush
+import time
+
+def load_data(file_name):
+    #Read in the frequency data.
+    
+    #f_heap is a heap of frequencies of the associated symbols
+    #f_dict is a dictionary with frequencies for keys and symbols for values
+    
+    f_heap = []
+    f_dict = defaultdict(set)
+    fn = open(file_name,'r')
+    nr_syms = int(fn.readline().strip())
+    sym_id = 0
+    
+    for line in fn:
+        f = int(line.strip())
+        f_dict[f].add(sym_id)
+        heappush(f_heap,f)
+        sym_id += 1
+    fn.close()
+    
+    assert len(f_heap) == len(set(f_heap)) #we will assume weights are distinct
+    return f_heap, f_dict
+
+def generate_coding(f_heap,f_dict):
+    #Generate Huffman coding by continuously popping the two smallest frequencies and merging their associated branches.
+    
+    f1 = heappop(f_heap)
+    f2 = heappop(f_heap)
+    if not f_heap:
+        Huffman_coding = dict()
+        Huffman_coding[max(f1,f2)]='0'
+        Huffman_coding[min(f1,f2)]='1'
+        return Huffman_coding
+    else:
+        fn = f1 + f2
+        if fn in f_dict:
+            raise ValueError('fatal error: frequency collision in coding')
+        f_dict[fn].add(f_dict[f1].pop())
+        f_dict[fn].add(f_dict[f2].pop())
+        heappush(f_heap,fn)
+        Huffman_coding = generate_coding(f_heap,f_dict)
+        bifur_code = Huffman_coding[fn]
+        Huffman_coding[max(f1,f2)] = bifur_code + '0'
+        Huffman_coding[min(f1,f2)] = bifur_code + '1'
+        del Huffman_coding[fn]
+    return Huffman_coding
+
+if __name__ == "__main__":
+    file_name = 'huffmaneasytest.txt'
+    sys.setrecursionlimit(1002) #for N pieces of data the recursion limit must be set at N+2
+    
+    start = time.time()
+
+    f_heap, f_dict = load_data(file_name)
+    Huffman_coding = generate_coding(f_heap,f_dict)
+
+    print('Huffman coding:')
+    print(Huffman_coding)
+    
+    end = time.time()
+    print(end - start)
